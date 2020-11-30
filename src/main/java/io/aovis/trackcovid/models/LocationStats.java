5f55be00191305cef13e83dbf9f52123900d8c71
Aovis <HowyIO@protonmail.com>
2020-11-30 00:00:01 -0600

    Started LocationStats

---
+++ src/main/java/io/aovis/trackcovid/models/LocationStats.java
@@ -0,0 +1,22 @@
+package io.aovis.trackcovid.models;
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+    private int latestTotalCases;
+
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return state;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
